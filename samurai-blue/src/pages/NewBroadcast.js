ad82f5598115dbfce33a5caa1ee55845a25eea23
wjlee <wenjin299@gmail.com>
2023-02-25 23:28:05 +0800

    broadcast button

--- samurai-blue/src/pages/NewBroadcast.js
+++ samurai-blue/src/pages/NewBroadcast.js
@@ -6,14 +6,13 @@ import { TwitchEmbed } from 'react-twitch-embed';
 
 function NewBroadcast() {
   return (
-    <div className="flex">
-    <div className="flex flex-col w-full pt-16 px-3 pr-6 pb-10 w-full gap-6">
+    <div className="flex gap-4 justify-start w-full relative">
+      <div className="flex flex-col w-full pt-16 gap-6">
+      <div className="page-heading w-full">New Broadcast</div>
+
       {/* <div className="bg-black w-[640px] h-[360px] rounded-xl"></div> */}
       <TwitchEmbed channel="Kiarakitty" hideControls withChat={false} height={360} width={640} />
       <div className="flex flex-col gap-4 w-[450px] flex-1">
-          <div className="text-3xl tracking-widest font-light">
-            New Broadcast
-          </div>
           <input
             type="text"
             name="broadcast-title"
@@ -30,7 +29,7 @@ function NewBroadcast() {
           />
       </div>
       </div>
-          <div className="featured-products w-[450px] h-[180px] flex flex-col">
+          <div className="featured-products flex w-auto absolute right-10 top-10 flex-col">
             <div className="text-xl tracking-widest font-semibold py-4 px-1">
               Featured Products
             </div>
@@ -38,7 +37,11 @@ function NewBroadcast() {
               <PlusIcon className="w-[24px] h-[24px]"></PlusIcon>
               Add Products
             </button>
-          </div>
+      </div>
+      <button className="dark-broadcast-btn flex gap-1 absolute bottom-8 right-20" onClick={}>
+             <PlayIcon className="w-[18px] h[20px]"></PlayIcon>
+             <Link to="/broadcasting">Start Broadcast</Link>
+          </button>
 </div>
 
 
