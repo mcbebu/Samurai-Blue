7bab0db90aa00535ca9fb8e866f7644ed64273db
wjlee <wenjin299@gmail.com>
2023-02-25 16:13:45 +0800

    login page updated

--- samurai-blue/src/App.js
+++ samurai-blue/src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
 import NavBar from './components/NavBar';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation} from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Orders from './pages/Orders';
 import Products from './pages/Products';
 import Settings from './pages/Settings';
 import NewBroadcast from './pages/NewBroadcast'
 import Broadcasts from './pages/Broadcasts';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+
 
 
 function App() {
+  const location = useLocation();
   return (
     <div className='flex w-full h-[100vh] justify-between font-opensans'>
-      <div className='hidden sm:flex'>
+      <div className={`hidden ${location.pathname.includes("login") || location.pathname.includes("sign-up") ? "hidden" : "sm:flex"}`}>
         <NavBar></NavBar>
       </div>
       <Routes>
@@ -23,6 +27,10 @@ function App() {
         <Route path="orders" element={<Orders/>}></Route>
         <Route path="products" element={<Products/>}></Route>
         <Route path="settings" element={<Settings />}></Route>
+        <Route path="login" element={<Login />}></Route>
+        <Route path="sign-up" element={<SignUp />}></Route>
+
+
       </Routes>
     </div>
   );
---
+++ samurai-blue/src/img/index.js
@@ -0,0 +1,8 @@
+import iglogo from "./instagram-logo.svg";
+import ttlogo from "./tiktok-logo.svg";
+import fblogo from "./facebook-logo.svg";
+import twitchlogo from "./twitch-logo.svg"
+
+
+
+export { iglogo, ttlogo, fblogo, twitchlogo };
